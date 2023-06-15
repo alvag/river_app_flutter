d5b6912b17d090576346c6e954dd6d7ef108a8b1
Max Alva Garcia <alva85@gmail.com>
2023-06-15 17:25:55 -0500

    Consumir un Future Provider

--- lib/presentation/provider/pokemon_provider.dart
+++ lib/presentation/provider/pokemon_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:river_app/config/config.dart';
 
-final pokemonProvider = FutureProvider<String>((ref) async {
-  return 'Ditto';
+final pokemonNameProvider = FutureProvider<String>((ref) async {
+  final name = await PokemonService.getPokemonName(1);
+  return name;
 });
--- lib/presentation/screens/future_provider/pokemon_screen.dart
+++ lib/presentation/screens/future_provider/pokemon_screen.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:river_app/presentation/provider/providers.dart';
 
-class PokemonScreen extends StatelessWidget {
+class PokemonScreen extends ConsumerWidget {
   const PokemonScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final pokemonName = ref.watch(pokemonNameProvider);
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text('FutureProvider'),
       ),
-      body: const Center(
-        child: CircularProgressIndicator(),
+      body: Center(
+        child: pokemonName.when(
+          data: (value) => Text(value),
+          error: (error, _) => Text('Error: $error.toString()'),
+          loading: () => const CircularProgressIndicator(),
+        ),
       ),
     );
   }
